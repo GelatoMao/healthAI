716a57ecfee6ecccb090861810427b7ee4c5ae39
GelatoMao <gelatomao@outlook.com>
2020-12-21 14:08:36 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+// 项目启动入口
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+// 创建vue根实例 把router配置到根实例中
+// 通过render方法把app根组件渲染到#app入口节点
 new Vue({
   router,
   render: h => h(App)
